d5b83d1902d94a801beb4a5e08051aa9d5e652dc
MichailShcherbakov <50011226+MichailShcherbakov@users.noreply.github.com>
2022-10-25 15:46:23 +0300

    Base UI project

--- packages/desktop-old/src/app.ts
+++ packages/desktop-old/src/app.ts
@@ -2,12 +2,10 @@ import { INestApplication } from "@nestjs/common";
 import { AppModule } from "./modules/app.module";
 import { createApp } from "./utils/createApp";
 
-let app: INestApplication;
-
 export async function bootstrap() {
   return await createApp(AppModule);
 }
 
-export async function shutdown() {
+export async function shutdown(app: INestApplication) {
   await app.close();
 }
